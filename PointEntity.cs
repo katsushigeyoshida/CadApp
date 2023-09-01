b2db4e6d3bdfc382be8f3846d958c34eedaeeb6b
Katsushige Yoshida <k-yosida@g06.itscom.net>
2023-09-02 08:12:09 +0900

    拡大縮小機能追加

--- PointEntity.cs
+++ PointEntity.cs
@@ -147,6 +147,17 @@ namespace CadApp
         }
 
         /// <summary>
+        /// 原点を指定して拡大縮小
+        /// </summary>
+        /// <param name="cp">原点</param>
+        /// <param name="scale">拡大率</param>
+        public override void scale(PointD cp, double scale)
+        {
+            mPoint.scale(cp, scale);
+            mArea = new Box(mPoint);
+        }
+
+        /// <summary>
         /// 要素のオフセット
         /// </summary>
         /// <param name="sp">始点座標</param>
